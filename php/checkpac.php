49671d824175d930728d85f199e7eaaa0731a946
ratjw <taweesak.jan@mahidol.ac.th>
2017-09-08 15:00:53 +0700

    end

--- php/checkpac.php
+++ php/checkpac.php
@@ -4,6 +4,7 @@
 	$ch = curl_init();                                 // set up curl
 	curl_setopt( $ch, CURLOPT_URL, $url );            // the url to request
 
+	if ( false===( $response = curl_exec( $ch ) ) ){	// fetch remote contents
 		$pacs = curl_error( $ch );                  	// doesn't exist
 	} else {
 		$pacs = "PAC";									// this url exists
