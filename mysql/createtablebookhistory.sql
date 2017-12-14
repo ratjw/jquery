d8d9c4e82860bd9089241c2a7c080ddff9ea34c6
ratjw <taweesak.jan@mahidol.ac.th>
2017-12-14 10:52:42 +0700

    bookhistory.sql

--- mysql/createtablebookhistory.sql
+++ mysql/createtablebookhistory.sql
@@ -1,9 +1,9 @@
 
 DROP TABLE IF EXISTS bookhistory;
 
-CREATE TABLE bookhistory LIKE book;
+CREATE TABLE bookh LIKE book;
 
-ALTER TABLE bookhistory 
+ALTER TABLE bookh 
 	MODIFY COLUMN qn int(10) UNSIGNED NOT NULL, 
 	DROP PRIMARY KEY, 
 	DROP KEY opdate, 
@@ -13,4 +13,4 @@ ALTER TABLE bookhistory
 	ADD revision INT(6) NOT NULL AUTO_INCREMENT AFTER action,
 	ADD editdatetime DATETIME NOT NULL AFTER revision,
 	ADD PRIMARY KEY (qn, revision),
-	ADD KEY (revision);
+	ADD KEY (revision,qn);
